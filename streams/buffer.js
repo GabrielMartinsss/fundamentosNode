6e39eea3b8c8238c2147487bbd3cdcbd1981dec2
Gabriel Martins <gabrielsilva3553@gmail.com>
2023-11-17 10:35:56 -0300

    to fix: get id from params

---
+++ streams/buffer.js
@@ -0,0 +1,6 @@
+// buffer => representaçao de um espaço na memoria do computador, 
+// usado especificamente para transitar dadods de maneira muito rapida.
+// pois os dados armazenados e lidos são no foramto binarios
+
+const buf = Buffer.from("hello")
+console.log(buf)
